5690d8b233091f939c81912bcfb2055232354542
mgsong <mgsong@jjsoft.kr>
2019-11-28 06:42:00 +0900

    add types of exported members

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,31 @@
-declare module "@mgsong/min-utils" {
-  export function getNewName(name: string, names: string[]): string
+declare module '@mgsong/min-utils' {
+  export function findById(_id: string): (list: any[]) => any
+  export function OR(
+    pred1: (arg: any) => boolean,
+    pred2: (arg: any) => boolean,
+  ): (arg: any) => boolean
+  export const AND: any
+  export const exclude: any
+  export const isNotNil: any
+  export const highlight: any
+  export const removeTag: any
+  export const peek: any
+  export const go: any
+  export const costant: any
+  export const noop: any
+  export const indexMap: any
+  export const idEqual: any
+  export const updateBy: any
+  export const removeBy: any
+  export const updateById: any
+  export const removeById: any
+  export const addLink: any
+  export const flatLog: any
+  export const forceFileDownload: any
+  export const download: any
+  export const getHostname: any
+  export const getProtocol: any
+  export const appendQueryParams: any
+  export const copyToClipboard: any
+  export const blinkDomElement: any
 }
