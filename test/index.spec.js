f7479d7d5162da299c2faa0e05f5add7b627914d
mgsong <mgsong@jjsoft.kr>
2020-01-12 23:13:25 +0900

    add testcase of AND

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-const { OR } = require("../src")
+const { OR, AND } = require("../src")
 const { expect } = require("chai")
 
 describe("test", () => {
@@ -9,5 +9,14 @@ describe("test", () => {
     expect(isZeroOrPositive(1)).to.be.equal(true)
     expect(isZeroOrPositive(0)).to.be.equal(true)
     expect(isZeroOrPositive(-1)).to.be.equal(false)
+  })
+
+  it("AND", () => {
+    const isPositive = num => num > 0
+    const isEven = num => num % 2 === 0
+    const isEvenAndPositive = AND(isPositive, isEven)
+    expect(isEvenAndPositive(2)).to.be.equal(true)
+    expect(isEvenAndPositive(0)).to.be.equal(false)
+    expect(isEvenAndPositive(-1)).to.be.equal(false)
   })
 })
