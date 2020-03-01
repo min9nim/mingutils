9477cccef696d790e728a5e2b89413204b96afdf
mgsong <mgsong@jjsoft.kr>
2020-03-02 06:40:02 +0900

    update

--- publish.sh
+++ publish.sh
@@ -1,3 +1,17 @@
+#!/bin/sh
+
+yarn test
+if [ $? -ne 0 ]; then
+  echo "test failed"
+  exit 1
+fi
+rm -r dist
+yarn build
+if [ $? -ne 0 ]; then
+  echo "build failed"
+  exit 1
+fi
+
 mv ~/.npmrc ~/.npmrc.tmp
 mv ~/.npmrc.mgsong ~/.npmrc
 npm publish
