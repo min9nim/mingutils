2d37bcbeb4c1a81fe597bdd74f39acefb91ff82b
min9nim <min9nim@gmail.com>
2020-06-10 08:39:43 +0900

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -12,8 +12,8 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-mv ~/.npmrc ~/.npmrc.tmp
-mv ~/.npmrc.mgsong ~/.npmrc
+# mv ~/.npmrc ~/.npmrc.tmp
+# mv ~/.npmrc.mgsong ~/.npmrc
 npm publish
-mv ~/.npmrc ~/.npmrc.mgsong
-mv ~/.npmrc.tmp ~/.npmrc
+# mv ~/.npmrc ~/.npmrc.mgsong
+# mv ~/.npmrc.tmp ~/.npmrc
